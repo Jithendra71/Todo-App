4978d9eb87ae8b87400ee529a7eed2b8a5eeb169
Jithendra71 <jithendra4567890@gmail.com>
2021-12-10 22:44:23 +0530

    Base code for Todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import  React, {useState } from 'react';
+import {Button, FormControl, InputLabel,Input} from '@mui/material'
+import Todolist from './Todolist';
+import Todo from './Todo';
 
 function App() {
+
+  const [todos,setTodos] = useState([])
+  const [input,setInput] = useState('')
+
+  const addTodo = (event)=>{
+    event.preventDefault()
+    setTodos([...todos, input])
+    setInput('')
+  }
+
+  const addInput = (event)=>{
+    setInput(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Todolist/> */}
+
+      <h1>Todo List</h1>
+      <form>
+      <FormControl>
+      <InputLabel>✍ Write a Todo to add</InputLabel>  
+      <Input type="text" placeholder="Enter a todo" value={input}  onChange={addInput}/>
+      <Button disabled={!input.trim()} onClick={addTodo} type='submit' variant="contained"  >Add todo</Button>
+      </FormControl>
+      
+      </form>
+      <ul>
+        {todos.map(todo => {
+          return <Todo todo={todo}/>
+        })}
+      </ul>
+
     </div>
   );
 }
